f2f122918365aa347b67e4d6a24e4c5eb657a378
Lawrence Ikhidero <43246123+lorens247@users.noreply.github.com>
2022-03-06 14:07:16 +0100

    cache cleared

--- core/config/database.php
+++ core/config/database.php
@@ -56,6 +56,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
+            'unix_socket'    => '/opt/lampp/var/mysql/mysql.sock',
             'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
